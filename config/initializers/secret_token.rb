42bdcb3ee3e9af37b96c4d22d8bce83242439fed
Tom Cartwright <tecartwright@gmail.com>
2013-06-03 16:32:33 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tomcartwright::Application.config.secret_token = '7701055c7b8ea0614d393178b184c04c9f6865380dbde8cde820db0010434fae855a13c5e3665a3c765215ef81965a9834d4e6abaa7103f9c40b5edc6d2d1d93'
---
+++ config/routes.rb
@@ -0,0 +1,5 @@
+Tomcartwright::Application.routes.draw do
+  root :to => 'pages#home'
+  match "/about" => "pages#about"
+  match "/contact" => "pages#contact"
+end
